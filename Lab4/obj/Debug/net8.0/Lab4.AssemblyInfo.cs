dd46776f660227309fa691eae93bf7686f66f7f4
H. Alperen Elbiz <94225207+alperenelbiz@users.noreply.github.com>
2024-03-20 11:48:04 +0300

    lab5 "added addition page link"

--- Lab4/obj/Debug/net8.0/Lab4.AssemblyInfo.cs
+++ Lab4/obj/Debug/net8.0/Lab4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+065482080513d47cec1760d7376264bfde4f9f85")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+838ca1c144cea1259d2195c69c45e8a3a37dc0a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
