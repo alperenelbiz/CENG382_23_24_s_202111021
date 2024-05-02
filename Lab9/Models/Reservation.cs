f5db00dde16e6bef58398157e3803645790f5e53
H. Alperen Elbiz <94225207+alperenelbiz@users.noreply.github.com>
2024-05-02 15:40:09 +0300

    created migration for Reservation.cs

---
+++ Lab9/Models/Reservation.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Lab9.Models
+{
+    public class Reservation
+    {
+        public int Id { get; set; }
+        public string CustomerName { get; set; }
+        public DateTime ReservationDate { get; set; }
+        // Add other properties as needed
+    }
+}
