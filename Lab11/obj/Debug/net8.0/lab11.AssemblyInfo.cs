bf98b93e4aea32cf5e62c43aa35d07ba17d3ee03
H. Alperen Elbiz <94225207+alperenelbiz@users.noreply.github.com>
2024-05-29 12:03:21 +0300

    quick update

--- Lab11/obj/Debug/net8.0/lab11.AssemblyInfo.cs
+++ Lab11/obj/Debug/net8.0/lab11.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab11")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+855e31e55df0fca225169e8027172c1a30379cbd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b632730c4604ab7c999c83512af4402021a3ee0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab11")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab11")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
